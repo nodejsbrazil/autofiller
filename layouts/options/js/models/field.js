53f7ccb1931b21e2d76e00875d7a9df200960e98
Lucas Santos <lhs.santoss@gmail.com>
2020-01-25 18:38:06 -0300

    Remove fields

--- layouts/options/js/models/field.js
+++ layouts/options/js/models/field.js
@@ -1,14 +1,21 @@
 class Field {
-  constructor (name, value) {
+  constructor (name, value, id) {
+    this.id = id || Date.now()
     this.name = name;
     this.value = value;
   }
 
   render () {
-    return `
-        Nome: ${this.name}
-        Value: ${this.value}
-      `
+    const list = document.createElement('li')
+    list.innerText = `Nome: ${this.name} | Valor: ${this.value}`
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.innerText = 'Remover'
+    deleteBtn.classList.add('btn-delete')
+    deleteBtn.setAttribute('data-id', this.id)
+
+    list.appendChild(deleteBtn)
+    return list
   }
 }
 
--- layouts/options/js/newField.js
+++ layouts/options/js/newField.js
@@ -1,9 +1,36 @@
-const addNewFieldButton = document.getElementById("btnAddNewField")
-const fields
+const $ = document.querySelector.bind(document)
+const addNewFieldButton = $('#btnAddNewField')
+const fields = $('#fields')
+
+window.onload = () => {
+  getSync(['fields'])
+    .then((results) => {
+      if (!results || !results.fields) return
+      for (result of results.fields) {
+        const field = new Field(result.name, result.value, result.id)
+        fields.appendChild(field.render())
+      }
+    })
+    .then(() => {
+      const deleteBtns = document.querySelectorAll('.btn-delete')
+
+      for (deleteBtn of deleteBtns) {
+        deleteBtn.onclick = performDelete
+      }
+    })
+}
+
+function performDelete ({ target }) {
+  const { id } = target.dataset
+  getSync(['fields']).then(results => {
+    const newFields = results.fields.filter(el => el.id !== parseInt(id))
+    setSync({ fields: newFields }).then(() => window.location.reload()).catch(console.error)
+  })
+}
 
 addNewFieldButton.onclick = () => {
-  const newFieldName = document.getElementById("intFieldName").value
-  const newFieldValue = document.getElementById("intFieldValue").value
+  const newFieldName = $('#intFieldName').value
+  const newFieldValue = $('#intFieldValue').value
 
   // Field is loaded from the page's HTML.
   const newField = new Field(newFieldName, newFieldValue)
@@ -20,12 +47,8 @@ addNewFieldButton.onclick = () => {
     // setSync is loaded from the page's HTML.
     setSync(updatedFields)
       .then(() => {
-        window.alert("Campo cadastrado com sucesso")
-        getSync(['fields']).then(console.log)
+        window.location.reload()
       })
-      .catch(err => {
-        console.log(err)
-      })
+      .catch(console.error)
   })
-
 }
